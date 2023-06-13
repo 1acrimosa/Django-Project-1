a551b9d4ba0e720e8d872fbaa6147b4700ab98ee
1acrimosa <dastanktl26@gmail.com>
2023-06-13 06:10:10 +0500

    updated

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request, 'main/index.html')
 
 
 def about(request):
