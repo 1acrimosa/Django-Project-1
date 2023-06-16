04bc09cab4347469f620f178ac37039f8ce54095
1acrimosa <dastanktl26@gmail.com>
2023-06-16 05:32:17 +0500

    added list of products to main page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Task
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Main page of this site', 'tasks': tasks})
 
 
 def about(request):
