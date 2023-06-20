7df10a50c7c35eba7ae518c1d90c94b53bf40529
1acrimosa <dastanktl26@gmail.com>
2023-06-21 02:10:21 +0500

    added item app

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Task',
+            name='Category',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
