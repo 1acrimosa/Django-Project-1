9ad76d9bec21ac48322e1fe25a75f1b5280aa94a
1acrimosa <dastanktl26@gmail.com>
2023-06-20 21:58:19 +0500

    Item information added

--- main/models.py
+++ main/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    title = models.CharField('Name', max_length=25)
-    task = models.TextField('Option, comments')
-
-    def __str__(self):
-        return self.title
+    name = models.CharField(max_length=255)
 
     class Meta:
-        verbose_name = 'Product'
+        ordering = ('name',)
         verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Task, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField('Name', max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    # image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
