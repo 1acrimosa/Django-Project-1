15995923aef91eb9ba7dda10e8805c94813f3638
1acrimosa <dastanktl26@gmail.com>
2023-06-21 01:21:02 +0500

    changed item logic

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Task(models.Model):
@@ -18,7 +19,7 @@ class Item(models.Model):
     name = models.CharField('Name', max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
-    # image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
