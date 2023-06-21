d781611c7ac6f2df0652af0497cb3b69e3893fee
1acrimosa <dastanktl26@gmail.com>
2023-06-21 17:07:53 +0500

    added plural name and name of objects

--- item/models.py
+++ item/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
