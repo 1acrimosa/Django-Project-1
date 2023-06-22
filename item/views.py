7934869f112704ac4f5dd5ca396a3c4e59202110
1acrimosa <dastanktl26@gmail.com>
2023-06-23 03:20:09 +0500

    fixed: size of pictures

--- item/views.py
+++ item/views.py
@@ -5,7 +5,9 @@ from .models import Item
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
 
     return render(request, 'item/detail.html', {
-        'item': item
+        'item': item,
+        'related_items': related_items
     })
