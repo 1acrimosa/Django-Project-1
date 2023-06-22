269303d2980a2c334ea41f3d791d19b7678e24e2
1acrimosa <dastanktl26@gmail.com>
2023-06-23 00:14:31 +0500

    added: item pages

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from core.views import index, about, why
+from django.urls import path, include
+from core.views import about, why
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('about-us/', about, name='about'),
     path('why/', why, name='why'),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
